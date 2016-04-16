c07f7b58336eeba34491d8488e78d2e9f81b1051
Mark Mendoza <markolo25@gmail.com>
2016-04-16 15:48:32 -0700

    I made a function then unmade a function that was there It works now

--- calculator/fac.hs
+++ calculator/fac.hs
@@ -1,15 +1,10 @@
-import Text.Read
-
-fac :: Int -> Int
-fac n = if n == 0 then 1 else n * fac (n - 1)
-        
+import Text.Read        
 getInt = do
       x <- getLine
       return (read x :: Int)      
 
-
-
 main = do
    putStrLn "What n would you like to n!"
-   fac getInt
-
+   n <- getInt
+   let fac n = if n == 0 then 1 else n * fac (n - 1)
+   print(fac n)
